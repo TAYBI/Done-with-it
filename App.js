eed4ac84f53290bf6ef7079f2be26d4c8472f576
TAYBI <taybib4@gmail.com>
2020-12-18 22:18:12 +0100

    flex

--- App.js
+++ App.js
@@ -1,41 +1,42 @@
-import {
-  useDeviceOrientation,
-  useDimensions,
-} from "@react-native-community/hooks";
 import React from "react";
-import {
-  Image,
-  Platform,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  View,
-} from "react-native";
+import { Platform, StatusBar, View } from "react-native";
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
   return (
-    // <View></View>
-    <SafeAreaView style={styles.container}>
+    <View
+      style={{
+        backgroundColor: "#f1f1f1",
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "flex-start",
+        justifyContent: "space-evenly",
+      }}
+    >
       <View
         style={{
-          width: "100%",
-          height: landscape ? "100%" : "30%",
-          backgroundColor: "dodgerblue",
+          backgroundColor: "tomato",
+          height: 100,
+          width: 100,
+          alignSelf: "center",
         }}
       ></View>
-    </SafeAreaView>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          // height: 100,
+          width: 100,
+          alignSelf: "stretch",
+        }}
+      ></View>
+      <View
+        style={{
+          backgroundColor: "gold",
+          alignSelf: "flex-end",
+          height: 100,
+          width: 100,
+        }}
+      ></View>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    color: "white",
-    backgroundColor: "#f1f1f1",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-  text: {
-    color: "white",
-  },
-});
