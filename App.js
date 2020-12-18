b25418df3dab4c2185fbaac128605c8bbfbf15b4
TAYBI <taybib4@gmail.com>
2020-12-18 22:31:45 +0100

    flex

--- App.js
+++ App.js
@@ -9,34 +9,47 @@ export default function App() {
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
         flex: 1,
         flexDirection: "row",
-        alignItems: "flex-start",
-        justifyContent: "space-evenly",
+        alignItems: "center",
+        alignContent: "center",
+        flexWrap: "wrap",
       }}
     >
       <View
         style={{
           backgroundColor: "tomato",
+          flexGrow: 1,
           height: 100,
           width: 100,
-          alignSelf: "center",
         }}
       ></View>
       <View
         style={{
           backgroundColor: "dodgerblue",
-          // height: 100,
+          height: 100,
           width: 100,
-          alignSelf: "stretch",
         }}
       ></View>
       <View
         style={{
           backgroundColor: "gold",
-          alignSelf: "flex-end",
           height: 100,
           width: 100,
         }}
       ></View>
+      {/* <View
+        style={{
+          backgroundColor: "green",
+          height: 300,
+          width: 100,
+        }}
+      ></View>
+      <View
+        style={{
+          backgroundColor: "orange",
+          height: 100,
+          width: 100,
+        }}
+      ></View> */}
     </View>
   );
 }
